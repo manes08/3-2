317ce4342e7dd19941f0b39a1d3ca2d6972a8243
manes08 <101406824+manes08@users.noreply.github.com>
2022-04-08 14:15:47 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    bomba.move(-1)
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        bomba.change(LedSpriteProperty.Y, 1)
+        basic.pause(100)
+    }
+    basic.showIcon(IconNames.Chessboard)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    bomba.set(LedSpriteProperty.X, 2)
+    bomba.set(LedSpriteProperty.Y, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    bomba.move(1)
+})
+let bomba: game.LedSprite = null
+bomba = game.createSprite(2, 2)
